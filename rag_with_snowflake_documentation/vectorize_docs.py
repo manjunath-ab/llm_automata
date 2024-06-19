7099c91f11286448e5d9b1d4154e29b7b8702acd
manjunath.ab <manjunath.ab@northeastern.edu>
2024-06-18 20:29:13 -0400

    next iter

---
+++ rag_with_snowflake_documentation/vectorize_docs.py
@@ -0,0 +1,23 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from website_pages_retreiver import getPagesFromSitemap
+
+urls = list(getPagesFromSitemap("https://docs.snowflake.com/en/"))
+
+docs = [WebBaseLoader(url).load() for url in urls]
+docs_list = [item for sublist in docs for item in sublist]
+
+text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=100, chunk_overlap=50
+)
+doc_splits = text_splitter.split_documents(docs_list)
+
+# Add to vectorDB
+vectorstore = Chroma.from_documents(
+    documents=doc_splits,
+    collection_name="rag-chroma",
+    embedding=OpenAIEmbeddings(),
+)
+retriever = vectorstore.as_retriever()
