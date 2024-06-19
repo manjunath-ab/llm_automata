87586d5575c835ff5bbabc40bf956befce2512dd
manjunath.ab <manjunath.ab@northeastern.edu>
2024-06-18 20:41:56 -0400

    replaced with ollama embeddings

--- rag_with_snowflake_documentation/vectorize_docs.py
+++ rag_with_snowflake_documentation/vectorize_docs.py
@@ -1,23 +1,26 @@
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
+from langchain_community.embeddings import OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from website_pages_retreiver import getPagesFromSitemap
 
 urls = list(getPagesFromSitemap("https://docs.snowflake.com/en/"))
-
-docs = [WebBaseLoader(url).load() for url in urls]
+print("Number of pages found: ", len(urls))
+print("loading pages...")
+docs = [WebBaseLoader(url).load() for url in urls[:10]]
 docs_list = [item for sublist in docs for item in sublist]
 
 text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
     chunk_size=100, chunk_overlap=50
 )
 doc_splits = text_splitter.split_documents(docs_list)
+print("Number of document splits: ", len(doc_splits))
 
 # Add to vectorDB
 vectorstore = Chroma.from_documents(
     documents=doc_splits,
     collection_name="rag-chroma",
-    embedding=OpenAIEmbeddings(),
+    embedding=OllamaEmbeddings(),
 )
 retriever = vectorstore.as_retriever()
+print(retriever)
